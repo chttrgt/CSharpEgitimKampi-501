d607e2969a12eab45b52435310d82e33ec07e2e0
Cihat Turgut <cihatturgut@yaani.com>
2025-01-14 01:05:11 +0300

    CEK501|21.12.2024_Dapper_Insert_Update_Delete

--- CSharpEgitimKampi-501/Form1.Designer.cs
+++ CSharpEgitimKampi-501/Form1.Designer.cs
@@ -43,6 +43,12 @@
             this.btnUpdate = new System.Windows.Forms.Button();
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblTotalProductCount = new System.Windows.Forms.Label();
+            this.lblMostExpensiveProduct = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblCategoryCount = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -54,6 +60,7 @@
             this.btnList.TabIndex = 0;
             this.btnList.Text = "Listele";
             this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_ClickAsync);
             // 
             // label1
             // 
@@ -152,6 +159,7 @@
             this.btnUpdate.TabIndex = 0;
             this.btnUpdate.Text = "Güncelle";
             this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // btnAdd
             // 
@@ -161,6 +169,7 @@
             this.btnAdd.TabIndex = 0;
             this.btnAdd.Text = "Ekle";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_ClickAsync);
             // 
             // btnDelete
             // 
@@ -170,13 +179,80 @@
             this.btnDelete.TabIndex = 0;
             this.btnDelete.Text = "Sil";
             this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(13, 249);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(210, 30);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Toplam Kitap Sayısı:";
+            // 
+            // lblTotalProductCount
+            // 
+            this.lblTotalProductCount.AutoSize = true;
+            this.lblTotalProductCount.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTotalProductCount.Location = new System.Drawing.Point(234, 250);
+            this.lblTotalProductCount.Name = "lblTotalProductCount";
+            this.lblTotalProductCount.Size = new System.Drawing.Size(25, 30);
+            this.lblTotalProductCount.TabIndex = 4;
+            this.lblTotalProductCount.Text = "0";
+            // 
+            // lblMostExpensiveProduct
+            // 
+            this.lblMostExpensiveProduct.AutoSize = true;
+            this.lblMostExpensiveProduct.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMostExpensiveProduct.Location = new System.Drawing.Point(445, 290);
+            this.lblMostExpensiveProduct.Name = "lblMostExpensiveProduct";
+            this.lblMostExpensiveProduct.Size = new System.Drawing.Size(25, 30);
+            this.lblMostExpensiveProduct.TabIndex = 4;
+            this.lblMostExpensiveProduct.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(278, 289);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(166, 30);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "En Pahalı Kitap:";
+            // 
+            // lblCategoryCount
+            // 
+            this.lblCategoryCount.AutoSize = true;
+            this.lblCategoryCount.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCategoryCount.Location = new System.Drawing.Point(234, 289);
+            this.lblCategoryCount.Name = "lblCategoryCount";
+            this.lblCategoryCount.Size = new System.Drawing.Size(25, 30);
+            this.lblCategoryCount.TabIndex = 4;
+            this.lblCategoryCount.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(59, 289);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(164, 30);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Kategori Sayısı:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSeaGreen;
-            this.ClientSize = new System.Drawing.Size(884, 306);
+            this.ClientSize = new System.Drawing.Size(884, 464);
+            this.Controls.Add(this.lblCategoryCount);
+            this.Controls.Add(this.lblMostExpensiveProduct);
+            this.Controls.Add(this.lblTotalProductCount);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.txtProductCategory);
             this.Controls.Add(this.txtProductStock);
             this.Controls.Add(this.label5);
@@ -194,6 +270,7 @@
             this.Controls.Add(this.btnList);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -217,6 +294,12 @@
         private System.Windows.Forms.Button btnUpdate;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblTotalProductCount;
+        private System.Windows.Forms.Label lblMostExpensiveProduct;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblCategoryCount;
+        private System.Windows.Forms.Label label8;
     }
 }
 
--- CSharpEgitimKampi-501/Form1.cs
+++ CSharpEgitimKampi-501/Form1.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using CSharpEgitimKampi_501.Dtos;
+using Dapper;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +18,72 @@ namespace CSharpEgitimKampi_501
         public Form1()
         {
             InitializeComponent();
+        }
+
+        SqlConnection cnn = new SqlConnection("Data Source=CIHATTURGUT\\SQLCHTTRGT;Initial Catalog=EgitimKampi501Db;Integrated Security=True");
+
+        private async void btnList_ClickAsync(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM tbl_Product";
+            var values = await cnn.QueryAsync<ResultProductDto>(query);
+            dataGridView1.DataSource = values.ToList();
+
+        }
+
+        private async void btnAdd_ClickAsync(object sender, EventArgs e)
+        {
+            string query = "INSERT INTO tbl_Product (ProductName,ProductStock ,ProductPrice,ProductCategory) VALUES (@ProductName,  @ProductStock, @ProductPrice,@ProductCategory)";
+
+            var parameters = new DynamicParameters();
+            parameters.Add("@ProductName", txtProductName.Text);
+            parameters.Add("@ProductStock", txtProductStock.Text);
+            parameters.Add("@ProductPrice", txtProductPrice.Text);
+            parameters.Add("@ProductCategory", txtProductCategory.Text);
+
+            await cnn.ExecuteAsync(query, parameters);
+            MessageBox.Show("Product Added");
+
+        }
+
+        private async void btnDelete_Click(object sender, EventArgs e)
+        {
+            string query = "DELETE FROM tbl_Product WHERE ProductId = @ProductId";
+            var parameters = new DynamicParameters();
+            parameters.Add("@ProductId", txtProductId.Text);
+            await cnn.ExecuteAsync(query, parameters);
+            MessageBox.Show("Product Deleted");
+
+        }
+
+        private async void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string query = "UPDATE tbl_Product SET ProductName = @ProductName, ProductStock = @ProductStock, ProductPrice = @ProductPrice, ProductCategory = @ProductCategory WHERE ProductId = @ProductId";
+            var parameters = new DynamicParameters();
+            parameters.Add("@ProductId", txtProductId.Text);
+            parameters.Add("@ProductName", txtProductName.Text);
+            parameters.Add("@ProductStock", txtProductStock.Text);
+            parameters.Add("@ProductPrice", txtProductPrice.Text);
+            parameters.Add("@ProductCategory", txtProductCategory.Text);
+            await cnn.ExecuteAsync(query, parameters);
+            MessageBox.Show("Product Updated");
+
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            string query = "SELECT Count(*) FROM tbl_Product";
+
+            var values = await cnn.QueryAsync<int>(query);
+            lblTotalProductCount.Text = values.FirstOrDefault().ToString();
+
+            string query2 = "SELECT ProductName FROM tbl_Product WHERE ProductPrice=(SELECT MAX(ProductPrice) FROM tbl_Product)";
+            var values2 = await cnn.QueryAsync<string>(query2);
+            lblMostExpensiveProduct.Text = values2.FirstOrDefault().ToString();
+
+            string query3 = "SELECT Count(Distinct(ProductCategory)) FROM tbl_Product";
+            var values3 = await cnn.QueryAsync<int>(query3);
+            lblCategoryCount.Text = values3.FirstOrDefault().ToString();
+
         }
     }
 }
