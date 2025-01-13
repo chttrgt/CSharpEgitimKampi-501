c60ea8e907bc083a10f223c2b69630350a9e4a99
Cihat Turgut <cihatturgut@yaani.com>
2025-01-14 00:01:03 +0300

    CEK501|14.12.2024_Dapper review done

---
+++ CSharpEgitimKampi-501/Repositories/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using CSharpEgitimKampi_501.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi_501.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<List<ResultProductDto>> GetAllProductAsync();
+        Task GetProductByIdAsync(int productId);
+        Task CreateProductAsync(CreateProductDto createProductDto);
+        Task UpdateProductAsync(UpdateProductDto updateProductDto);
+        Task DeleteProductAsync(int productId);
+    }
+}
---
+++ CSharpEgitimKampi-501/Repositories/ProductRepository.cs
@@ -0,0 +1,37 @@
+﻿using CSharpEgitimKampi_501.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi_501.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task CreateProductAsync(CreateProductDto createProductDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProductAsync(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultProductDto>> GetAllProductAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetProductByIdAsync(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProductAsync(UpdateProductDto updateProductDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
